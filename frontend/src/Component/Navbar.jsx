7ea67789a926e3432b7b2d8f532907c7cd1be4ed
w3-80717 <chavan1609@gmail.com>
2025-07-18 18:25:58 +0530

    Menu icon added

--- frontend/src/Component/Navbar.jsx
+++ frontend/src/Component/Navbar.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css"
 
 function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+
     return (
         <div className='main-container'>
             <div className='container'>
-                <nav className="navbar-nav">
+                <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <nav className={`navbar-nav${menuOpen ? ' open' : ''}`}>
+
                     <img
                         src="https://www.advik.co.in/wp-content/uploads/2025/04/25.svg"
                         alt="25"
@@ -19,7 +28,7 @@ function Navbar() {
                         <span className="dropdown-icon">
                             {/* Down arrow SVG */}
                             <svg width="12" height="12" viewBox="0 0 20 20" fill="none">
-                                <path d="M5 8L10 13L15 8" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
+                                <path d="M5 8L10 13L15 8" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
                             </svg>
                         </span>
                     </a>
@@ -27,7 +36,7 @@ function Navbar() {
                         TECHNOLOGY
                         <span className="dropdown-icon">
                             <svg width="12" height="12" viewBox="0 0 20 20" fill="none">
-                                <path d="M5 8L10 13L15 8" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
+                                <path d="M5 8L10 13L15 8" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
                             </svg>
                         </span>
                     </a>
